6f9f86cca99ef93fe66022218d35ab1a412c0b03
John Roberson <johnroberson2015@gmail.com>
2019-05-05 20:49:37 -0400

    Create grades_by_dept_treemap.png

--- images/skills_map.R
+++ images/skills_map.R
@@ -1,1 +1,23 @@
-f
+library(ggplot2)
+
+df <- 
+
+ggplot(population_df_color, aes(area = area, subgroup = region, fill = fill)) +
+  geom_treemap(color = "white", size = 0.5*.pt, alpha = NA) + 
+  geom_treemap_subgroup_text(
+    family = dviz_font_family,
+    colour = "white",
+    place = "centre", alpha = 0.7,
+    grow = TRUE
+  ) +
+  geom_treemap_subgroup_border(color = "white") +
+  geom_treemap_text(
+    aes(label = state),
+    color = "black",
+    family = dviz_font_family,
+    place = "centre",
+    grow = FALSE
+  ) +
+  scale_fill_identity() +
+  coord_cartesian(clip = "off") +
+  guides(colour = "none", fill = "none")
